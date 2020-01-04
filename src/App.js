5da6824c038b4665e52b6d9bab5ad459cc4fcf03
woc917 <woc917@hotmail.com>
2020-01-04 15:23:22 +0900

    change_class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+    
+//       <h1>Hello world!</h1>
+    
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
-      <h1>Hello world!</h1>
-    
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  // 상태값
+  // 함수
+  // 라이프 사이클
+
+  render() {
+
+    // 리턴에서 사용될 상태값, 함수에 대한 상수 선언
+
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
+    )
+
+  }
+}
